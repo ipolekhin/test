14fc3070e15c7895ba1c5c02b9707b11761eaf95
Иван <ipolekhin@yadnex.ru>
2019-11-18 16:57:33 +0300

    001
    1. Организована выборка цвета
    2.  Доработано взаимодействие между собой, корзины в шапке и в теле.

--- js/script.js
+++ js/script.js
@@ -185,48 +185,63 @@ $(function() {
         }
     });
 
+    //пересчет
+    function recalculationCountSum() {
+        var  price_sum = 0, sum = 0, block = $('.basket_tooltip-list');
+        //итоговая сумма
+        block.find('td.price').each(function() {
+            price_sum += parseInt($(this).html());
+        });
+        $('.amount-price span').html(price_sum + ' ₽');
+        // количество в корзине
+        block.find('input').each(function(i, input) {
+            sum += parseInt($(input).val());
+        });
+        $('.order_item_count').html(sum);
+    }
+
     // Количество товаров quantity
-    $('.quantity button').click(function () {
-        var $this = $(this).parent().find('input'), btn = $(this), qty = parseFloat($this.val()), min = 1, max = 999, new_qty;
+    $('.quantity button').click(function (e) {
+        e.preventDefault();
+        var $this = $(this).parent().find('input'),
+            btn = $(this), qty = parseFloat($this.val()),
+            min = 1, max = 999, new_qty,
+            elementid = btn.parents('.goods__item').attr('data-element_id');
+
         if(btn.hasClass('plus')) {
             new_qty = (qty < max) ? qty + 1 : qty;
         } else {
             new_qty = (qty > min) ? qty - 1 : qty;
         }
-        $this.val(new_qty).change();
+
+        $('.goods__item[data-element_id="'+elementid+'"]').each(function() {
+            var product_price = $(this).find('td.price');
+            $(this).find('input').val(new_qty).change();
+            product_price.text(product_price.data('price') * new_qty + ' ₽');
+        });
+        recalculationCountSum();
     });
 
-    // Пересчет суммы и количество товаров quantity basket
-    $('.quantity.basket button, .quantity.basket .goods__remove').click(function (e) {
-        var btn = $(this), price_sum = 0, sum = 0;
-        var count = btn.siblings('.goods__count').find('input').val();
-        var product_price = btn.parents('td').siblings('td.price');
+    //удаление товара из корзины
+    $('.quantity.basket .goods__remove').click(function (e) {
         if ($(e.target).closest('.quantity.basket .goods__remove').length) {
             e.stopPropagation();
             e.preventDefault();
+            var elementid = $(this).parents('.goods__item').attr('data-element_id');
+            $('.goods__item[data-element_id="'+elementid+'"]').each(function() {
+                $(this).remove();
+            });
             var tooltipcount = $('.tooltip_count span');
             var countitem = tooltipcount.attr('data-count') - 1;
             tooltipcount.attr('data-count', countitem);
-            btn.parents('tr').remove();
             if (countitem > 0) {
                 tooltipcount.text('В корзине еще '+ countitem + ' товаров');
             }
             else {
                 tooltipcount.text('');
             }
+            recalculationCountSum();
         }
-        $(product_price).text(product_price.data('price') * count + ' ₽');
-        $('.basket_tooltip-list').find('td.price').each(function() {
-            price_sum += parseInt($(this).html());
-        });
-        // количество в корзине
-        $('.basket_tooltip-list').find('input').each(function(i, input) {
-            sum += parseInt($(input).val());
-        });
-        // Итоговая сумма
-        $('.amount-price span').html(price_sum + ' ₽');
-        // Количество в корзине
-        $('.order_item_count').html(sum);
     });
 
     // Фото товара
@@ -244,6 +259,7 @@ $(function() {
         });
     });
 
+    //customSelect
     function customSelect(el){
         var options = [],
             option = $(el).children('option'),
@@ -254,42 +270,37 @@ $(function() {
         });
         $(el).after('<ul class="custom-select" data-selected-value="' + options[0] + '">');
         customSelect = $(el).siblings('.custom-select');
-        $(customSelect).append('<li class="selected-option"><span>' + options[0] + '</span>');
-        $(customSelect).children('.selected-option').append('<ul class="options">');
+        customSelect.append('<li class="selected-option"><span>' + options[0] + '</span>');
+        customSelect.children('.selected-option').append('<ul class="options">');
 
         for(var i = 1; i < options.length; i++) {
-            $(customSelect).find('.options').append('<li class="' +
+            customSelect.find('.options').append('<li class="' +
                 $(option[i]).prop('class') + '">' + options[i] + '</li>');
         }
 
-
-        for (var i = 1; i < options.length; i++) {
-            $(custommultiSelect).next('.options').append('<li data-value="'+ options[i] +'">' +
-                '<i  class="' + $(option[i]).prop('class') + '"' + ' ></i></li>');
-        }
-
-        $(customSelect).click(function(){
+        customSelect.click(function(){
             $(this).toggleClass('open');
             $('.options',this).toggleClass('open');
         });
-        $(customSelect).find('.options li').click(function(){
+
+        customSelect.find('.options li').click(function(){
             var selection = $(this).text();
             var dataValue = $(this).attr('data-value');
-            var selected = $(customSelect).find('.selected-option span').text(selection);
+            var selected = customSelect.find('.selected-option span').text(selection);
             for(var i = 1; i < option.length; i++) {
                 if($(option[i]).text() === selected.text()) {
                     $(option[i]).attr('selected', 'true');
                     $(option[i]).siblings().removeAttr('selected');
                 }
             };
-            $(customSelect).attr('data-selected-value',dataValue);
+            customSelect.attr('data-selected-value',dataValue);
         });
     };
     customSelect('#select-category');
     customSelect('#select-collection');
     customSelect('#select-post');
 
-    //Multiselect
+    // Multiselect
     function custommultiSelect(el){
         var options = [],
             option = $(el).children('option'),
@@ -298,36 +309,50 @@ $(function() {
         $(option).each(function(){
             options.push($(this).html());
         });
-        $(el).after('<ul class="custom-select" data-selected-value="' + options[0] + '">');
+        $(el).after('<ul class="custom-select">');
         custommultiSelect = $(el).siblings('.custom-select');
-        $(custommultiSelect).append('<li class="selected-option"><span>' + options[0] + '</span>');
-        $(custommultiSelect).after('<ul class="options">');
+        custommultiSelect.append('<li class="selected-option"><span>' + options[0] + '</span>');
+        custommultiSelect.after('<ul class="options">');
 
         for (var i = 1; i < options.length; i++) {
-            $(custommultiSelect).next('.options').append('<li data-value="'+ options[i] +'">' +
+            custommultiSelect.next('.options').append('<li data-value="' + $(option[i]).val() +'">' +
                 '<i  class="' + $(option[i]).prop('class') + '"' + ' ></i></li>');
         }
-        $(custommultiSelect).click(function(){
+
+        //Выборка цвета
+        function selectedColor() {
+            custommultiSelect.find('.selected-option').html('<span>' + options[0] + '</span>');
+            var optionselected = $(el).children('option:selected');
+            optionselected.each(function (i) {
+                custommultiSelect.find('.selected-option span').append('<i class="'+ $(optionselected[i]).prop('class') + '"></i>');
+                console.log($(optionselected[i]));
+            });
+        }
+        selectedColor();
+
+        custommultiSelect.click(function(){
             $(this).toggleClass('open');
             $(this).next('.options').toggleClass('open');
             $(this).next('.options.open').find('li').each(function(index){
-                index++
+                index++;
                 if($(option[index]).prop('selected')) {
                     $(option[index]).prop('selected', true);
                     $(this).addClass('focus');
                 }
             });
         });
-        $(custommultiSelect).next('.options').find('li').click(function(){
+        custommultiSelect.next('.options').find('li').click(function(){
             var dataValue = $(this).attr('data-value');
             for(var i = 1; i < option.length; i++) {
-                if($(option[i]).text() === dataValue) {
+                if($(option[i]).val() === dataValue) {
                     if($(option[i]).prop('selected')) {
                         $(option[i]).prop('selected', false);
                         $(this).removeClass('focus');
+                        selectedColor();
                     } else {
                         $(option[i]).prop('selected', true);
                         $(this).addClass('focus');
+                        selectedColor();
                     }
                 }
             };
